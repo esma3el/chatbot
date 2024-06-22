7cc236fc830c43d4b729502297d853dac29f441d
root <root@DESKTOP-P5V2TBP>
2024-06-22 23:42:50 +0400

    update

--- server.py
+++ server.py
@@ -2,17 +2,21 @@ from green_api import send_message
 from flask import Flask ,request,Response
 from waitress import serve
 
+c1="0943ec77cfca5d75c8e91d1495666da6a8731378e9a680c04035dc066f628070"
+c2="94010ef1a494e4975900bb6406cbd0e23e8300b5661ceb27b671e5fb449ef71d"
+KEYS = [c1,c2]
+
+
 app = Flask(__name__)
 
 @app.post("/bot/160ae567-a19e-442c-9c54-1954f3c968e1")
 def chatbot_route():
+    key = request.headers.get("Chat-Key")
+    if key not in KEYS:
+        return Response("Error : UNAUTHORIZED ACCESS\n",status=401)
     msg = request.json
     response = send_message(msg)
     return Response("success.",status=200)
-
-@app.get("/")
-def index():
-    return "hi ismail mosa ibrahim daif"
 
 if __name__ == "__main__":
     # app.run(debug=True)
